695a865fcf53ba8f6219b3be0ba524a6a657b00a
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-06-27 08:36:15 +0200

    fixing packerror and creating Board.

--- Client/capstone_client/src/Components/Board.tsx
+++ Client/capstone_client/src/Components/Board.tsx
@@ -1,1 +1,15 @@
+import React from "react";
+import "../styles/board.css"
 
+
+const xAxis: Array<String> = ["a","b","c","d","e","f","g","h",];
+const yAxis: Array<String> = ["1","2","3","4","5","6","7","8",];
+
+export default function Board(){
+    return(
+    <div className="chessboard">
+
+    </div>
+    )
+
+}
