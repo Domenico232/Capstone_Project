01b26adfac8de50cec74890b415931786a38aa0e
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-06-27 12:28:19 +0200

    pieces fix

--- Client/capstone_client/src/Components/Board.tsx
+++ Client/capstone_client/src/Components/Board.tsx
@@ -11,7 +11,34 @@ interface Piece {
 }
 
 const pieces: Piece[] = []
-pieces.push({image : "", x:1, y:7})
+for (let e = 0; e < xAxis.length; e++) {    
+    //pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg", x:e, y:6})
+    pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg", x:e, y:1})
+}
+for (let e = 0; e < xAxis.length; e++) {    
+    pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg", x:e, y:6})
+}
+//rooks
+pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg", x:7, y:7})
+pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg", x:0, y:7})
+pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg", x:0, y:0})
+pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg", x:7, y:0})
+//kingts
+pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg", x:1, y:7})
+pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg", x:6, y:7})
+pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg", x:1, y:0})
+pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg", x:6, y:0})
+//bishops
+pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg", x:5, y:7})
+pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg", x:2, y:7})
+pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg", x:5, y:0})
+pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg", x:2, y:0})
+//queens/kings
+pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg", x:4, y:7})
+pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg", x:4, y:0})
+pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg", x:3, y:0})
+pieces.push({image : "https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg", x:3, y:7})
+
 
 export default function Board(){
     let board = [];
@@ -20,7 +47,7 @@ export default function Board(){
             const number= i+j+2
             let image = undefined;
             pieces.forEach(p=>{
-                if (p.x === j && p.y===i) {
+                if (p.x === i && p.y===j) {
                     image = p.image
                 }
             });
--- Client/capstone_client/src/Components/Cells.tsx
+++ Client/capstone_client/src/Components/Cells.tsx
@@ -8,11 +8,11 @@ export default function Cells({number,image}:Props){
 
      if ((number%2)===0) {
                 
-                return <span className="cell black"><img className="piece" src={image} alt="piece" /></span>
+                return <span className="cell black">{image? <img className="piece" src={image} alt="piece" />: <></>}</span>
             
             }else{
                
-                return<span className="cell white"><img className="piece" src={image}  alt="piece" /></span>
+                return<span className="cell white">{image? <img className="piece" src={image} alt="piece" />: <></>}</span>
                 
             }
 }
