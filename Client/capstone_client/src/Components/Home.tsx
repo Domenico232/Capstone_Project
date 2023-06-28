a4d35bfde1659de7924b8162d627514655b254cc
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-06-28 16:13:09 +0200

    .

--- Client/capstone_client/src/Components/Home.tsx
+++ Client/capstone_client/src/Components/Home.tsx
@@ -1,12 +1,20 @@
+import { Col, Row } from "react-bootstrap";
 import ChessGame from "./Chessgame";
 
 const Home:React.FC = () =>{
     return (
       <>
-      <ChessGame/>
-      <div className="infobox">
+      <Row>
+        <Col xs md={1} lg={2}></Col>
+        <Col xs={12} md={8} lg={6}><ChessGame /></Col>
+        <Col xs md={3} lg={4} >
+            <div>
 
-      </div>
+            </div>
+
+        </Col>
+      </Row>
+
       </>
     );
   }
