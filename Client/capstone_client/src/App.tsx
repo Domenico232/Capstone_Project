3d9052c978cd4297176e908b3aa63ac246e13ddf
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-06-28 11:37:50 +0200

    Router implementation

--- Client/capstone_client/src/App.tsx
+++ Client/capstone_client/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import Chessgame from './Components/Chessgame';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div id='App'>
-    <Chessgame />
+
+      <div id='App'>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/ciao" element={<Chessgame />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </div>
+    
   );
 }
 
