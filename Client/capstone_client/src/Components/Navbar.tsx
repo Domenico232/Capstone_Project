b86f0d69e1f8a0a30c68acdcac3aebce249de012
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-06-28 14:53:30 +0200

    .

--- Client/capstone_client/src/Components/Navbar.tsx
+++ Client/capstone_client/src/Components/Navbar.tsx
@@ -5,20 +5,20 @@ const NavbarTop:React.FC = () =>{
   return (
     <Navbar expand="lg" className="bg-dark topBar">
          <Container>
-        <Navbar.Brand href="#home">ChillChess</Navbar.Brand>
+        <Navbar.Brand >ChillChess</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link >Home</Nav.Link>
+            <Nav.Link >Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+              <NavDropdown.Item >Action</NavDropdown.Item>
+              <NavDropdown.Item >
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item >Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item >
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
