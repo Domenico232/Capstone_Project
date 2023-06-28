ea52bb35678a8d7ad0473d4e3ab3a08cd2974f67
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-06-28 15:20:04 +0200

    navbarcolor

--- Client/capstone_client/src/Components/Navbar.tsx
+++ Client/capstone_client/src/Components/Navbar.tsx
@@ -1,9 +1,9 @@
 import { Container, Nav, NavDropdown } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
-
+import "./assets.componets.style/navbar.css"
 const NavbarTop:React.FC = () =>{
   return (
-    <Navbar expand="lg" className="bg-dark topBar">
+    <Navbar expand="lg" className="topBar">
          <Container>
         <Navbar.Brand >ChillChess</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
