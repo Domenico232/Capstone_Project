a541d3b493ef943c69bbc552a683f42ba05b7d21
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-07-07 10:28:10 +0200

    firebase impl 1

--- Client/capstone_client/src/Components/Chessgame.tsx
+++ Client/capstone_client/src/Components/Chessgame.tsx
@@ -1,253 +1,290 @@
 import React, { useState } from "react";
-import "./assets.componets.style/board.css"
+import "./assets.componets.style/board.css";
 import Square from "./Square";
 //white pieces
-const wPawn:string="https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg"
-const wKnight:string="https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg";
-const wBishop:string="https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg";
-const wRook:string="https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg";
-const wKing:string="https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg";
-const wQueen:string="https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg";
+const wPawn: string =
+  "https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg";
+const wKnight: string =
+  "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg";
+const wBishop: string =
+  "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg";
+const wRook: string =
+  "https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg";
+const wKing: string =
+  "https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg";
+const wQueen: string =
+  "https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg";
 //black pieces
-const bPawn:string="https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg"
-const bKnight:string="https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg";
-const bBishop:string="https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg";
-const bRook:string="https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg";
-const bKing:string="https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg";
-const bQueen:string="https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg";
+const bPawn: string =
+  "https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg";
+const bKnight: string =
+  "https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg";
+const bBishop: string =
+  "https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg";
+const bRook: string =
+  "https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg";
+const bKing: string =
+  "https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg";
+const bQueen: string =
+  "https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg";
 interface ChessMoveData {
-    id: number;
-    primo: ChessPlayer;
-    piecerow: number;
-    piececol: number;
-    moverow: number;
-    movecol: number;
-    secondo: ChessPlayer;
-  }
-  
-  interface ChessPlayer {
-    id: number;
-    name: string;
-    username: string;
-    email: string;
-    password: string;
-    lastName: string;
-    dataInserimento: string;
-    elo: number;
-    nationality: string;
-    roles: UserRole[];
-  }
-  
-  interface UserRole {
-    id: number;
-    roleName: string;
-  }
+  id: number;
+  primo: ChessPlayer;
+  piecerow: number;
+  piececol: number;
+  moverow: number;
+  movecol: number;
+  secondo: ChessPlayer;
+}
+
+interface ChessPlayer {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+  lastName: string;
+  dataInserimento: string;
+  elo: number;
+  nationality: string;
+  roles: UserRole[];
+}
+
+interface UserRole {
+  id: number;
+  roleName: string;
+}
 
 const ChessGame: React.FC = () => {
-    const [boardState, setBoardState] = useState<Array<Array<string | null>>>([
-       // Starting Chessboard
-        ["R", "P", null, null, null, null, "p", "r"],
-        ["N", "P", null, null, null, null, "p", "n"],
-        ["B", "P", null, null, null, null, "p", "b"],
-        ["Q", "P", null, null, null, null, "p", "q"],
-        ["K", "P", null, null, null, null, "p", "k"],
-        ["B", "P", null, null, null, null, "p", "b"],
-        ["N", "P", null, null, null, null, "p", "n"],
-        ["R", "P", null, null, null, null, "p", "r"],
-      ]);
-const [selectedPieces, setSelectedPiece] = useState<{ row: number; col: number } | null>(null);
-const isItFirstTime : Array<Array<boolean | null>> =[
-       // Starting Chessboard pawns
-        [null, false, null, null, null, null, false,null],
-        [null, false, null, null, null, null, false,null],
-        [null, false, null, null, null, null, false,null],
-        [null, false, null, null, null, null, false,null],
-        [null, false, null, null, null, null, false,null],
-        [null, false, null, null, null, null, false,null],
-        [null, false, null, null, null, null, false,null],
-        [null, false, null, null, null, null, false,null],
-    ];
+  const [isYourTurn, setIsYourTurn] = useState<Boolean>(false);
+  const [black, setblack] = useState<Boolean>(false);
+  const [boardState, setBoardState] = useState<Array<Array<string | null>>>([
+    // Starting Chessboard
+    ["R", "P", null, null, null, null, "p", "r"],
+    ["N", "P", null, null, null, null, "p", "n"],
+    ["B", "P", null, null, null, null, "p", "b"],
+    ["Q", "P", null, null, null, null, "p", "q"],
+    ["K", "P", null, null, null, null, "p", "k"],
+    ["B", "P", null, null, null, null, "p", "b"],
+    ["N", "P", null, null, null, null, "p", "n"],
+    ["R", "P", null, null, null, null, "p", "r"],
+  ]);
+  const [selectedPieces, setSelectedPiece] = useState<{
+    row: number;
+    col: number;
+  } | null>(null);
+  const isItFirstTime: Array<Array<boolean | null>> = [
+    // Starting Chessboard pawns
+    [null, false, null, null, null, null, false, null],
+    [null, false, null, null, null, null, false, null],
+    [null, false, null, null, null, null, false, null],
+    [null, false, null, null, null, null, false, null],
+    [null, false, null, null, null, null, false, null],
+    [null, false, null, null, null, null, false, null],
+    [null, false, null, null, null, null, false, null],
+    [null, false, null, null, null, null, false, null],
+  ];
 
-    const fetchOppoMove = async (id: number) => {
-        const response = await fetch(`http://localhost:8080/chessboard/${id}`);
-        const jsonData: ChessMoveData = await response.json();
-        console.log(jsonData);
-        console.log("pezzo row, pezzo col + move row, move col " + jsonData.piecerow, jsonData.piececol, jsonData.moverow, jsonData.movecol);
-        fakeMove(jsonData.piecerow, jsonData.piececol, jsonData.moverow, jsonData.movecol);
-      };
-
-      
-      
-      const goOn = () => {
-        fetchOppoMove(1);
-      };
-      
-      const fakeMove = (row: number, col: number, newRow: number, newCol: number) => {
-        setSelectedPiece({ row, col });
-        handleSquareClick(newRow, newCol, { row, col }); // Passa il selectedPiece come argomento
-      };
-      
-      const handleSquareClick = (row: number, col: number, selectedPiece?: { row: number, col: number }) => {
-        console.log(row, col);
-        console.log(selectedPiece || selectedPiece); // Utilizza il selectedPiece passato come argomento
-      
-        if (selectedPieces) {
-          if (isMoveValid(selectedPieces.row, selectedPieces.col, row, col)) {
-            movePiece(selectedPieces.row, selectedPieces.col, row, col);
-          }
-          setSelectedPiece(null);
-        } else {
-          const piece = boardState[row][col];
-          console.log(piece);
-          if (piece !== null) {
-            setSelectedPiece({ row, col });
-          }
-        }
-      };
-
-      
-
-console.log("BOARDSTATE "+boardState);
-
-const renderSquare = (row: number, col: number) => {
-    const isBlack = (row + col) % 2 === 1;
-        const piece = boardState[row][col];
-            let image = "";
-        if (piece === "p") {
-            image = wPawn;
-        }else if(piece === "P"){
-            image = bPawn;
-        }else if(piece === "R"){
-            image = bRook;
-        }else if(piece === "r"){
-            image = wRook;
-        }else if(piece === "n"){
-            image = wKnight;
-        }else if(piece === "N"){
-            image = bKnight;
-        }else if(piece === "b"){
-            image = wBishop;
-        }else if(piece === "B"){
-            image = bBishop;
-        }else if(piece === "q"){
-            image = wQueen;
-        }else if(piece === "Q"){
-            image = bQueen;
-        }else if(piece === "k"){
-            image = wKing;
-        }else if(piece === "K"){
-            image = bKing;
-        }else{
-            image="";
-        }
-        return (
-            <Square
-          key={`${row}-${col}`}
-          isBlack={isBlack}
-          piece={piece}
-          image={image}
-          onClick={() => handleSquareClick(row, col)}
-          />
-          );
-        };
-
-        const renderBoard = () => {
-            const rows = boardState.map((row, rowIndex) => (
-                <div className="chessrow" key={rowIndex}>
-              {row.map((_, colIndex) => renderSquare(rowIndex, colIndex))}
-
-            </div>
-          ));
-          return <div className="boardes">{rows}</div>;
-        };
-
-        const isMoveValid = (startRow: number, startCol: number, endRow: number, endCol: number) => {
-            const piece = boardState[startRow][startCol]
-            const pieceMove = boardState[endRow][endCol]
-            if (piece==="p") {
-                    if (pieceMove===null) {
-                        if (!(startRow-endRow===0)) {
-                            return false;
-                        }
-                        if (endCol>startCol) {
-                            return false
-                        }
-                        if (isItFirstTime[startRow][startCol]===false) {
-                            
-                            if(startCol-endCol>2) {
-                                return false
-                            }
-                            isItFirstTime[startRow][startCol]=true;
-                        } else if (startCol-endCol>1){
-                            return false
-                        }
-                        
-                         //if pawn go on populated cell
-                    }else{
-                        console.log("verify full cell_________________")
-                        console.log((startRow-endRow))
-                        console.log("&&")
-                        console.log((startCol-endCol))
-    
-                        if (!(startRow-endRow===-1 && startCol-endCol===1)) {
-                            if ((startRow-endRow===1 && startCol-endCol===1)) {
-                                return true
-                            }
-                            return false     
-                        } 
-                        
-                    }
-                
-           
-      }else if (piece==="P") {
-        if (pieceMove===null) {
-            if (!(startRow-endRow===0)) {
-                return false;
-            }
-            if (endCol<startCol) {
-                return false
-            }
-            if(startCol-endCol<-2) {
-                    return false
-             }
-             //if pawn go on populated cell
-        }else{
-            console.log("verify full cell_________________")
-            console.log((startRow-endRow))
-            console.log("&&")
-            console.log((startCol-endCol))
-
-            if (!(startRow-endRow===-1 && startCol-endCol===-1)) {
-                if ((startRow-endRow===1 && startCol-endCol===-1)) {
-                    return true
-                }
-                return false     
-            } 
-            
-        }
-      }
-
-          return true;
-        };
-      
-        const movePiece = (startRow: number, startCol: number, endRow: number, endCol: number) => {
-          const newBoardState = [...boardState];
-          console.log(newBoardState);
-          const piece = newBoardState[startRow][startCol];
-          newBoardState[startRow][startCol] = null;
-          newBoardState[endRow][endCol] = piece;
-          setBoardState(newBoardState);
-        };
-
-
-
-
-    return (
-      <div className="chess-game">
-        <h1 onClick={goOn}>Chess Game</h1>
-        {renderBoard()}
-      </div>
+  const fetchOppoMove = async (id: number) => {
+    const response = await fetch(`http://localhost:8080/chessboard/${id}`);
+    const jsonData: ChessMoveData = await response.json();
+    console.log(jsonData);
+    console.log(
+      "pezzo row, pezzo col + move row, move col " + jsonData.piecerow,
+      jsonData.piececol,
+      jsonData.moverow,
+      jsonData.movecol
+    );
+    fakeMove(
+      jsonData.piecerow,
+      jsonData.piececol,
+      jsonData.moverow,
+      jsonData.movecol
     );
   };
+
+  const goOn = () => {
+    fetchOppoMove(1);
+  };
+
+  const fakeMove = (
+    row: number,
+    col: number,
+    newRow: number,
+    newCol: number
+  ) => {
+    setSelectedPiece({ row, col });
+    handleSquareClick(newRow, newCol, { row, col }); // Passa il selectedPiece come argomento
+  };
+
+  const handleSquareClick = (
+    row: number,
+    col: number,
+    selectedPiece?: { row: number; col: number }
+  ) => {
+    console.log(row, col);
+    console.log(selectedPiece || selectedPiece); // Utilizza il selectedPiece passato come argomento
+
+    if (selectedPieces) {
+      if (isMoveValid(selectedPieces.row, selectedPieces.col, row, col)) {
+        movePiece(selectedPieces.row, selectedPieces.col, row, col);
+      }
+      setSelectedPiece(null);
+    } else {
+      const piece = boardState[row][col];
+      console.log(piece);
+      if (piece !== null) {
+        setSelectedPiece({ row, col });
+      }
+    }
+  };
+
+  console.log("BOARDSTATE " + boardState);
+
+  const renderSquare = (row: number, col: number) => {
+    const isBlack = (row + col) % 2 === 1;
+    const piece = boardState[row][col];
+    let image = "";
+    if (piece === "p") {
+      image = wPawn;
+    } else if (piece === "P") {
+      image = bPawn;
+    } else if (piece === "R") {
+      image = bRook;
+    } else if (piece === "r") {
+      image = wRook;
+    } else if (piece === "n") {
+      image = wKnight;
+    } else if (piece === "N") {
+      image = bKnight;
+    } else if (piece === "b") {
+      image = wBishop;
+    } else if (piece === "B") {
+      image = bBishop;
+    } else if (piece === "q") {
+      image = wQueen;
+    } else if (piece === "Q") {
+      image = bQueen;
+    } else if (piece === "k") {
+      image = wKing;
+    } else if (piece === "K") {
+      image = bKing;
+    } else {
+      image = "";
+    }
+    return (
+      <Square
+        key={`${row}-${col}`}
+        isBlack={isBlack}
+        piece={piece}
+        image={image}
+        onClick={() => handleSquareClick(row, col)}
+      />
+    );
+  };
+
+  const renderBoard = () => {
+    const rows = boardState.map((row, rowIndex) => (
+      <div className="chessrow" key={rowIndex}>
+        {row.map((_, colIndex) => renderSquare(rowIndex, colIndex))}
+      </div>
+    ));
+    return <div className="boardes">{rows}</div>;
+  };
+
+  const isMoveValid = (
+    startRow: number,
+    startCol: number,
+    endRow: number,
+    endCol: number
+  ) => {
+    const piece = boardState[startRow][startCol];
+    const pieceMove = boardState[endRow][endCol];
+
+    if (!black) {
+      if (piece === "p") {
+        if (pieceMove === null) {
+          if (!(startRow - endRow === 0)) {
+            return false;
+          }
+          if (endCol > startCol) {
+            return false;
+          }
+          if (isItFirstTime[startRow][startCol] === false) {
+            if (startCol - endCol > 2) {
+              return false;
+            }
+            isItFirstTime[startRow][startCol] = true;
+          } else if (startCol - endCol > 1) {
+            return false;
+          }
+
+          //if pawn go on populated cell
+        } else {
+          console.log("verify full cell_________________");
+          console.log(startRow - endRow);
+          console.log("&&");
+          console.log(startCol - endCol);
+
+          if (!(startRow - endRow === -1 && startCol - endCol === 1)) {
+            if (startRow - endRow === 1 && startCol - endCol === 1) {
+              return true;
+            }
+            return false;
+          }
+        }
+      }
+    }else if (piece === "P") {
+      if (pieceMove === null) {
+        if (!(startRow - endRow === 0)) {
+          return false;
+        }
+        if (endCol < startCol) {
+          return false;
+        }
+        if (startCol - endCol < -2) {
+          return false;
+        }
+        //if pawn go on populated cell
+      } else {
+        console.log("verify full cell_________________");
+        console.log(startRow - endRow);
+        console.log("&&");
+        console.log(startCol - endCol);
+
+        if (!(startRow - endRow === -1 && startCol - endCol === -1)) {
+          if (startRow - endRow === 1 && startCol - endCol === -1) {
+            return true;
+          }
+          return false;
+        }
+      }
+    }
+
+    return true;
+  };
+
+  const movePiece = (
+    startRow: number,
+    startCol: number,
+    endRow: number,
+    endCol: number
+  ) => {
+    const newBoardState = [...boardState];
+    console.log(newBoardState);
+    const piece = newBoardState[startRow][startCol];
+    newBoardState[startRow][startCol] = null;
+    newBoardState[endRow][endCol] = piece;
+    setBoardState(newBoardState);
+  };
+
+  return (
+    <div className="chess-game">
+      <h1 onClick={goOn}>Chess Game</h1>
+      {isYourTurn ? <div>your turn</div> : <div>Opponent turn</div>}
+      {renderBoard()}
+    </div>
+  );
+};
 
 export default ChessGame;
--- Client/capstone_client/src/Components/Home.tsx
+++ Client/capstone_client/src/Components/Home.tsx
@@ -1,18 +1,14 @@
-import { Col, Row } from "react-bootstrap";
-import ChessGame from "./Chessgame";
 
+import ChessGame from "./Chessgame";
 const Home:React.FC = () =>{
     return (
       <>
-      <Row>
-        <Col xs={12} md={7} lg={6}><ChessGame /></Col>
-        <Col xs={12} md={12} lg={4} >
+
+          <ChessGame />
             <div className="infobox">
 
             </div>
 
-        </Col>
-      </Row>
 
       </>
     );
